f63904f5a529cd687432da685755f2fc417c3173
faiza10 <faizakurniagck@gmail.com>
2023-02-13 11:26:30 +0700

    tutorial3

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const root = document.getElementById("root");
 
 linkHome.onclick= function (event){
     event.preventDefault();
-    root.textContent = "Welcome to Home";
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
     history.pushState(null, "", event.target.href);
 };
 
@@ -13,6 +15,20 @@ linkAbout.onclick= function (event){
     root.textContent = "Welcome to About";
     history.pushState(null, "", event.target.href);
 };
+
+function HomeScreen(){
+    const input = document.createElement("input");
+    input.placeholder = "Enter your name";
+
+    const textPreview = document.createElement("p");
+    textPreview.textContent = "Hello";
+
+    const div = document.createElement("div");
+    div.append(input);
+    div.append(textPreview);
+
+    return div;
+}
 
 if (location.hash == "#about") {
     root.textContent = "Welcome to About";
